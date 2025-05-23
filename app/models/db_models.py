1c10b399961964e4aad0a9532a439a6a9a4fcb21
Mubaraq Odumeso <mubskill@gmail.com>
2025-05-23 16:35:05 -0500

    added new tables

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -19,7 +19,7 @@ class Organizations(Base):
 class Users(Base):
     __tablename__ = "users"
     user_id = Column(Integer, primary_key=True, server_default=text("floor(random() * 1000000 + 1)::int"), nullable=False)
-    org_id = Column(Integer, ForeignKey("organizations.org_id"), nullable=False)
+    org_id = Column(Integer, ForeignKey("organizations.org_id", ondelete="CASCADE"), nullable=False)
     first_name = Column(String(200), nullable=False)
     last_name = Column(String(200), nullable=False)
     user_email = Column(String(200), nullable=False, unique=True)
@@ -31,10 +31,33 @@ class Users(Base):
 class Tasks(Base):
     __tablename__ = "tasks"
     task_id = Column(Integer, primary_key=True, server_default=text("floor(random() * 1000000 + 1)::int"), nullable=False)
-    org_id = Column(Integer, ForeignKey("organizations.org_id"), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    org_id = Column(Integer, ForeignKey("organizations.org_id",  ondelete="CASCADE"), nullable=False)
+    task_name = Column(String, nullable=False)
+    task_description = Column(String, nullable=True)
+    created_on = Column(TIMESTAMP(timezone = True), nullable=False
+                        ,server_default=text('now()'))
+class TaskAssignment(Base):
+    __tablename__ = "task_assignments"
+    task_id = Column(Integer, primary_key=True, server_default=text("floor(random() * 1000000 + 1)::int"), nullable=False)
+    org_id = Column(Integer, ForeignKey("organizations.org_id",  ondelete="CASCADE"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    task_name = Column(String, nullable=False)
+    task_description = Column(String, nullable=True)
+    due_date = Column(String, nullable=False)
+    task_status = Column(String, nullable=False)
+    proof_of_completion = Column(String, nullable = False)
+    created_on = Column(TIMESTAMP(timezone = True), nullable=False
+                        ,server_default=text('now()'))
+    
+class AUditLog(Base):
+    __tablename__ = "audit_log"
+    assignment_id = Column(Integer, primary_key=True, server_default=text("floor(random() * 1000000 + 1)::int"), nullable=False)
+    task_id = Column(Integer, ForeignKey("organizations.org_id",  ondelete="CASCADE"), nullable=False)
+    org_id = Column(Integer, ForeignKey("organizations.org_id",  ondelete="CASCADE"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
     task_name = Column(String, nullable=False)
     task_description = Column(String, nullable=True)
     task_status = Column(String, nullable=False)
-    created_on = Column(TIMESTAMP(timezone = True), nullable=False
+    completed_on = Column(TIMESTAMP(timezone = True), nullable=False
                         ,server_default=text('now()'))
+    assigned_on = Column(TIMESTAMP(timezone = True), nullable=False)
